0e16a9b6e6a865d3022a95e75db09759b7f0595c
lala <lala9990929@gmail.com>
2020-11-14 23:29:31 +0800

    path

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -3,13 +3,13 @@
 <div class="story_content">
   <h1 class="title">{{story.title}}</h1>
   <template v-if="story.image01">
-    <img :src="`/images/story/${story.image01}`" class="img-fluid"/>
+    <img :src="`/storyDesign/dist/images/story/${story.image01}`" class="img-fluid"/>
   </template>
   <template v-if="story.image02">
-    <img :src="`/images/story/${story.image02}`" class="img-fluid"/>
+    <img :src="`/storyDesign/dist/images/story/${story.image02}`" class="img-fluid"/>
   </template>
   <template v-if="story.image03">
-    <img :src="`/images/story/${story.image03}`" class="img-fluid"/>
+    <img :src="`/storyDesign/dist/images/story/${story.image03}`" class="img-fluid"/>
   </template>
 </div>
 </section>
@@ -27,7 +27,7 @@ export default {
   },
   methods: {
     getStories() {
-      this.$http.get('/api/db.json').then((res) => {
+      this.$http.get('/storyDesign/dist/api/db.json').then((res) => {
         res.data.stories.forEach((item) => {
           if (item.id === Number(this.$route.params.id)) {
             this.story = item;
